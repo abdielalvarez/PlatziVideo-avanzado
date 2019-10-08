a199827844b18dd9b3f988d6bc4d36e4e614478a
Abdiel <abdielalvarez@icloud.com>
2019-10-08 03:01:09 -0500

    Redux terminado, sin reto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,57 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => (
-  <header className="header">
-    <img className="header__img" src={logo} alt="Platzi Video" />
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
-      </div>
-      <ul>
-        <li><a href="/">Cuenta</a></li>
-        <li><a href="/">Cerrar Sesión</a></li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = (props) => {
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
 
-export default Header;
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+
+  return (
+    <header className='header'>
+      {/* El componente Link es un sustituto mejorado de la etiqueta <a href=''> de html, la diferencia está en que el efecto de recarga no es tan obvio (desagradable) al usuario */}
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='Platzi Video' />
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          {hasUser ?
+            <img src={gravatar(user.email)} alt={user.email} /> :
+            <img src={userIcon} alt='' />
+          }
+          <p>Perfil</p>
+        </div>
+        <ul>
+          {hasUser ?
+            <li><a href='/'>{user.name}</a></li> :
+            null
+          }
+          {hasUser ?
+            <li><a href='#logout' onClick={handleLogout}>Cerrar Sesión</a></li> :
+            <li><Link to='/login'>Iniciar Sesión</Link></li>
+          }
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
